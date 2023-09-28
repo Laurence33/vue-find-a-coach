3abbab42a7f8b08c6f95acc09b6bc1e3a11216fb
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-28 22:15:20 +0800

    vuex: contact coach

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
+
 export const store = createStore({
-  modules: { coaches: coachesModule },
+  modules: { coaches: coachesModule, requests: requestsModule },
   state() {
     return {
       userId: Date.now(),
