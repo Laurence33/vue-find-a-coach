6f4ebd0a181e7ed0f81108f6b604e08d0e7aa9aa
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-28 22:06:56 +0800

    bug fix contact link

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -9,7 +9,7 @@
     <base-card>
       <header>
         <h2>Interested? Reach out now!</h2>
-        <base-button link to="contactLink">Contact</base-button>
+        <base-button link :to="contactLink">Contact</base-button>
       </header>
       <router-view></router-view>
     </base-card>
@@ -34,7 +34,7 @@ export default {
       return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
     },
     contactLink() {
-      return this.$route.path + '/' + this.id + '/contact';
+      return this.$route.path + '/contact';
     },
     areas() {
       return this.selectedCoach.areas;
