32e7806840efb73f9e9e0d2c5b026f3b9e2f39b7
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-10-15 21:49:10 +0800

    fixup

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,32 @@
+export default {
+  login() {},
+  async signup(context, payload) {
+    const body = {
+      email: payload.email,
+      password: payload.password,
+      returnSecureToken: true,
+    };
+    const url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=' +
+      process.env.FIREBASE_KEY;
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+    context.commit('setUser', {
+      token: responseData.idToken,
+      tokenExpiration: responseData.expiresIn,
+      userId: responseData.localId,
+      email: responseData.email,
+    });
+  },
+};
---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,17 @@
+import getters from './getters.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
+
+export default {
+  state() {
+    return {
+      userId: null,
+      token: null,
+      tokenExpiration: null,
+      email: null,
+    };
+  },
+  getters,
+  mutations,
+  actions,
+};
