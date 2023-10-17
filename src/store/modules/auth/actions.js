65c6c8fe63875bce35bae4aac3306c4878302a85
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-10-18 07:24:43 +0800

    auto-login: store token in localStorage

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,13 +1,25 @@
 export default {
   async login(context, payload) {
+    return context.dispatch('auth', { ...payload, mode: 'login' });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', { ...payload, mode: 'signup' });
+  },
+  async auth(context, payload) {
+    const { mode } = payload;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+      process.env.VUE_APP_FIREBASE_KEY;
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        process.env.VUE_APP_FIREBASE_KEY;
+    }
     const body = {
       email: payload.email,
       password: payload.password,
       returnSecureToken: true,
     };
-    const url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-      process.env.VUE_APP_FIREBASE_KEY;
     const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(body),
@@ -20,6 +32,10 @@ export default {
       );
       throw error;
     }
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('email', responseData.email);
+    localStorage.setItem('tokenExpiration', responseData.expiresIn);
     context.commit('setUser', {
       token: responseData.idToken,
       tokenExpiration: responseData.expiresIn,
@@ -27,34 +43,22 @@ export default {
       email: responseData.email,
     });
   },
-  async signup(context, payload) {
-    const body = {
-      email: payload.email,
-      password: payload.password,
-      returnSecureToken: true,
-    };
-    const url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-      process.env.VUE_APP_FIREBASE_KEY;
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(body),
-    });
-
-    const responseData = await response.json();
-    console.log(responseData);
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to authenticate.'
-      );
-      throw error;
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const email = localStorage.getItem('email');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+    if (token && userId) {
+      context.commit('setUser', {
+        token,
+        userId,
+        email,
+        tokenExpiration,
+      });
+      return true;
     }
-    context.commit('setUser', {
-      token: responseData.idToken,
-      tokenExpiration: responseData.expiresIn,
-      userId: responseData.localId,
-      email: responseData.email,
-    });
+
+    return false;
   },
   async logout(context) {
     context.commit('setUser', {
